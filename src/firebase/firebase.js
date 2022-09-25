42d1d8800d5a6a523c1fe59575fcf304d4fb5adc
Shubham Soni <shubhamsoni611@gmail.com>
2022-09-26 02:10:00 +0530

    Add files via upload

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+// import firebase from "firebase";
+
+// var firebaseApp = firebase.initializeApp({
+//     apiKey: "AIzaSyDEJHSeZDX8JHHLlLBZHblpo0_lpCp-q6A",
+//       authDomain: "openauto-shubham.firebaseapp.com",
+//       projectId: "openauto-shubham",
+//       storageBucket: "openauto-shubham.appspot.com",
+//       messagingSenderId: "278329378183",
+//       appId: "1:278329378183:web:371d9ac3d13baf308cbf22",
+//       measurementId: "G-CRDSP4L5YM"
+// });
+
+// var db = firebaseApp.firestore();
+
+// export { db };
+
